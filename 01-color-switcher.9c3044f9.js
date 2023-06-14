51e2f1cc5f03b11e4e444efadf815a085012efcf
Tamira2804 <122819496+Tamira2804@users.noreply.github.com>
2023-06-14 21:01:18 +0000

    Deploying to gh-pages from @ Tamira2804/goit-js-hw-09@20b681f8b1c0095e9aacbe66f2a0689f7461a605 🚀

---
+++ 01-color-switcher.9c3044f9.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null,o=!1;t.addEventListener("click",(function(){o||(e=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}),1e3),o=!0)})),n.addEventListener("click",(function(){clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.9c3044f9.js.map
