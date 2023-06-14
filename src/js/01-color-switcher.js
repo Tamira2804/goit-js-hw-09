20b681f8b1c0095e9aacbe66f2a0689f7461a605
Tamira2804 <tamira2804@gmail.com>
2023-06-14 23:00:41 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+let isActive = false;
+
+startBtn.addEventListener('click', () => {
+  if (isActive) {
+    return;
+  }
+
+  startColorChange();
+  isActive = true;
+});
+
+function startColorChange() {
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtn.addEventListener('click', () => {
+  stopColorChange();
+});
+
+function stopColorChange() {
+  clearInterval(intervalId);
+}
